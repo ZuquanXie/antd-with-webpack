0ad8c70cacb461ee770db6002ed7f55945aca67d
zuquanxie <designer_xzq@163.com>
2018-07-23 10:40:15 +0800

    调整webpack.config.js结构，去除发布版的source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,19 @@
 const path = require('path');
-const isDevMode = process.env.NODE_ENV === 'development';
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
-/* 自定义服务 */
-const customServer = require('./src/server/index.js');
-
 /* less全局变量 */
 const lessModifyVars = require('./src/theme/lessModifyVars');
 
-/* 插件 */
-const plugins = [
-  new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src/templates/index.html'),
-    favicon: path.resolve(__dirname, 'src/public/favicon.ico')
-  })
-];
-
-/* 代码分块 */
-const optimization = {
-  runtimeChunk: 'single',
-  splitChunks: {
-    chunks: 'all',
-    name: true,
-    cacheGroups: {
-      vendor: {
-        test: /[\\/]node_modules[\\/]/,
-        name: 'vendors',
-        chunks: 'all'
-      }
-    }
-  }
-};
-
-/* 开发模式 */
-if (process.env.NODE_ENV === 'development') {
-  plugins.push(new webpack.HotModuleReplacementPlugin())
-}
-
-/* 发布模式 */
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(new CleanWebpackPlugin([path.resolve(__dirname, 'build')]));
-  plugins.push(new MiniCssExtractPlugin({ filename: '[name].css', chunkFilename: '[id].css' }));
-
-  optimization.minimizer = [
-    new UglifyJsWebpackPlugin({
-      sourceMap: true
-    })
-  ];
-}
-
-module.exports = {
+const config = {
   entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    filename: isDevMode ? '[name].js' : '[chunkHash].js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/'
-  },
-  mode: process.env.NODE_ENV,
-  devtool: isDevMode ? 'cheap-module-eval-source-map' : 'source-map',
-  devServer: {
-    port: 3000,
-    contentBase: path.resolve(__dirname, 'src/public'),
-    before: customServer,
-    hot: true,
-    /* 访问内容的重写（当http请求的地址匹配不到内容时，根据重写规则重写返回内容，可用于解决前端路由页面刷新时路由不匹配的问题） */
-    historyApiFallback: true // 简单配置
-    // historyApiFallback: {
-    //   rewrites: [
-    //     { from: /\*/, to: '/' }
-    //   ]
-    // }
-  },
   module: {
     rules: [
       {
-        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
         test: /\.js$/,
         use: [{
           loader: 'babel-loader',
@@ -91,14 +26,6 @@ module.exports = {
         }]
       },
       {
-        test: /\.(le|c)ss$/,
-        use: [
-          { loader: isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader },
-          'css-loader',
-          { loader: 'less-loader', options: { javascriptEnabled: true, modifyVars: lessModifyVars } }
-        ]
-      },
-      {
         test: /\.(jpg|png|svg|gif|ico|mp4)$/,
         use: [
           { loader: 'file-loader', options: { outputPath: 'static' } }
@@ -106,6 +33,95 @@ module.exports = {
       }
     ]
   },
-  plugins,
-  optimization
 };
+
+/* 插件 */
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src/templates/index.html'),
+    favicon: path.resolve(__dirname, 'src/public/favicon.ico')
+  })
+];
+
+/* 代码分块 */
+const optimization = {};
+
+/* 开发模式 */
+if (process.env.NODE_ENV === 'development') {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  config.mode = 'development';
+  config.output = {
+    filename: '[name].js',
+    path: '/build',
+    publicPath: '/',
+    pathinfo: false
+  };
+  config.devtool = 'cheap-module-eval-source-map';
+  config.devServer = {
+    port: 3000,
+      contentBase: path.resolve(__dirname, 'src/public'),
+      before: require(path.resolve(__dirname, 'src/server/index.js')),
+      hot: true,
+      /* 访问内容的重写（当http请求的地址匹配不到内容时，根据重写规则重写返回内容，可用于解决前端路由页面刷新时路由不匹配的问题） */
+      historyApiFallback: true // 简单配置
+    // historyApiFallback: {
+    //   rewrites: [
+    //     { from: /\*/, to: '/' }
+    //   ]
+    // }
+  };
+  config.module.rules.push({
+    test: /\.(le|c)ss$/,
+    use: [
+      { loader: 'style-loader' },
+      'css-loader',
+      { loader: 'less-loader', options: { javascriptEnabled: true, modifyVars: lessModifyVars } }
+    ]
+  });
+  config.plugins = plugins;
+  config.optimization = optimization;
+}
+
+/* 发布模式 */
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new CleanWebpackPlugin([path.resolve(__dirname, 'build')]));
+  plugins.push(new MiniCssExtractPlugin({ filename: '[name].css', chunkFilename: '[id].css' }));
+
+  optimization.runtimeChunk = 'single';
+  optimization.splitChunks = {
+    chunks: 'all',
+    name: true,
+    cacheGroups: {
+      vendor: {
+        test: /[\\/]node_modules[\\/]/,
+        name: 'vendors',
+        chunks: 'all'
+      }
+    }
+  };
+  optimization.minimizer = [
+    new UglifyJsWebpackPlugin({
+      sourceMap: false
+    })
+  ];
+
+  config.mode = 'production';
+  config.output = {
+    filename: '[chunkHash].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/'
+  };
+  config.module.rules.push({
+    test: /\.(le|c)ss$/,
+    use: [
+      { loader: MiniCssExtractPlugin.loader },
+      'css-loader',
+      { loader: 'less-loader', options: { javascriptEnabled: true, modifyVars: lessModifyVars } }
+    ]
+  });
+  config.plugins = plugins;
+  config.optimization = optimization;
+}
+
+module.exports = config;
